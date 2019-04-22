21740d41ca7778f951cac18e5b25c87813921de7
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-22 11:03:39 -0600

    pushes on errors

--- wild5/src/App.js
+++ wild5/src/App.js
@@ -1,27 +1,15 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native'
-import { Header, Card, CardSection, Button, Input, Spinner } from './common'
-<<<<<<< HEAD
-import LoginForm from './LoginForm';
+import { Header, Card, CardSection, Button, Input, Spinner } from './components/common';
+import LoginForm from './components/LoginForm'
 
 class App extends Component{
-    state = {
-
-    }
-    
-=======
-
-class App extends Component{
->>>>>>> f26889fe93e89bd90313ff9232217e110a43b0ad
 
     render(){
         return (
             <View>
                 <Header text='Wild5'/>
-<<<<<<< HEAD
                 <LoginForm />
-=======
->>>>>>> f26889fe93e89bd90313ff9232217e110a43b0ad
             </View>
         )
     }
