510dc7853e45854a355855a0673a9dc6f27f93d6
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-22 10:21:17 -0600

    deleted google api key

--- wild5/src/components/App.js
+++ wild5/src/components/App.js
@@ -1,26 +1,21 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native'
 import { Header, Card, CardSection, Button, Input, Spinner } from './common'
+import LoginForm from './LoginForm';
 
 class App extends Component{
     state = {
 
     }
-    componentWillMount(){
-        firebase.initializeApp({
-            apiKey: "AIzaSyC93k0KGpd8myVQxCTgWPw6Qk9NzNA6b_o",
-            authDomain: "wild5-5ca8b.firebaseapp.com",
-            databaseURL: "https://wild5-5ca8b.firebaseio.com",
-            projectId: "wild5-5ca8b",
-            storageBucket: "wild5-5ca8b.appspot.com",
-            messagingSenderId: "714885268112"
-          })
-    }
+    
 
     render(){
         return (
             <View>
                 <Header text='Wild5'/>
+                <CardSection>
+                    <LoginForm />
+                </CardSection>
             </View>
         )
     }
