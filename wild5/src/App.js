2688a1abe158b53177d6253eeaa067ccd557d1ce
William Violette <violettewj@gmail.com>
2019-04-22 10:26:30 -0600

    push on context

---
+++ wild5/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native'
+import { Header, Card, CardSection, Button, Input, Spinner } from './common'
+
+class App extends Component{
+
+    render(){
+        return (
+            <View>
+                <Header text='Wild5'/>
+            </View>
+        )
+    }
+}
+
+export default App;
---
+++ wild5/src/context/context.js
@@ -0,0 +1,22 @@
+import React from 'react'
+const { Consumer, Provider } = React.createContext();
+
+export default class GlobalProvider extends Component {
+    state = {
+        
+    }
+    render() {
+        return (
+          <Provider value={{
+            ...this.state
+          }}>
+            {this.props.children}
+          </Provider>
+        )
+      }
+    }
+      export function withProvider(C) {
+        return props => <Consumer>
+          {value => <C {...value}{...props} />}
+        </Consumer>
+}
