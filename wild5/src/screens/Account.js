87e654aa66e3f5df2597eb4f6d8b1b9e73e2ddbe
William Violette <violettewj@gmail.com>
2019-04-23 13:55:47 -0600

    push

---
+++ wild5/src/screens/Account.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Account extends Component {
+  render() {
+    return (
+     <View>
+       <Text>
+         Account
+       </Text>
+     </View>
+    )
+  }
+}
+
+export default Account
---
+++ wild5/src/screens/Quests.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Quests extends Component {
+  render() {
+    return (
+        <View>
+       <Text>
+         Quests
+       </Text>
+     </View>
+    )
+  }
+}
+
+export default Quests
