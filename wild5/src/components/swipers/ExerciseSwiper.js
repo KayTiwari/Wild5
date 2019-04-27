7eb29fe8db6a3551fa623c73c419201614b3b5b1
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-26 22:18:44 -0600

    trashed the chinese swiper npm b.s., instead used horizontal scroll view for the Exercise I lesson, next to implement the questions + state changes

--- wild5/src/components/swipers/ExerciseSwiper.js
+++ wild5/src/components/swipers/ExerciseSwiper.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+ 
 import Swiper from 'react-native-swiper';
  
-class ExerciseSwiper extends Component {
+const styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
+ 
+export default class ExerciseSwiper extends Component {
   render(){
     return (
       <Swiper style={styles.wrapper} showsButtons={true}>
@@ -19,33 +53,5 @@ class ExerciseSwiper extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-    wrapper: {
-    },
-    slide1: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#9DD6EB',
-    },
-    slide2: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#97CAE5',
-    },
-    slide3: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#92BBD9',
-    },
-    text: {
-      color: '#fff',
-      fontSize: 30,
-      fontWeight: 'bold',
-    }
-  })
-
-export default ExerciseSwiper;
+ 
+AppRegistry.registerComponent('wild5', () => Swiper);
