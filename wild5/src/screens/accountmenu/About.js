5c4ca20289895a546b91fadb08dcf46a3c72d779
William Violette <violettewj@gmail.com>
2019-04-30 11:47:11 -0600

    profile page and then some

---
+++ wild5/src/screens/accountmenu/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text } from 'react-native'
+
+function About() {
+  return (
+    <View>
+     <Text>About</Text>
+ </View>
+  )
+}
+
+
+export default About
