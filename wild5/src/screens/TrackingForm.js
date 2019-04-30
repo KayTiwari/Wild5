af2683a82373f984728741eade2ad73e9f386fb6
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-30 10:14:36 -0600

    added welcome screen to trackingform

--- wild5/src/screens/TrackingForm.js
+++ wild5/src/screens/TrackingForm.js
@@ -18,7 +18,7 @@ class TrackingForm extends Component{
         return (
             <ScrollView pagingEnabled={true}>
             <View style={{
-                    backgroundColor: '#5f9ea0',
+                    backgroundColor: '#36A0B3',
                     flex: 1,
                     width: screenwidth,
                     height: screenheight,
@@ -26,24 +26,24 @@ class TrackingForm extends Component{
                     alignItems: 'center'
                 }}>
                 <Text style={{
+                    fontSize: 40,
+                    padding: 15,
+                    textAlign: 'center',
+                    color: 'white'}}>Welcome!</Text>
+                <Text style={{
                     fontSize: 30,
                     padding: 50,
                     textAlign: 'center',
+                    marginBottom: 150,
                     color: 'white'
-                    }}>Exercise Lesson I</Text>
-                    <Text style={{
-                    fontSize: 40,
-                    padding: 15,
-                    marginBottom: 200,
-                    textAlign: 'center',
-                    color: 'white'}}>FID</Text>
+                    }}>Wellness Tracking Form</Text>
                 </View>
 
                 <View style={{
                     backgroundColor: '#5f9ea0',
                     flex: 1,
                     width: screenwidth,
-                    height: screenheight.
+                    height: screenheight,
                     justifyContent: 'center',
                     alignItems: 'center'
                 }}>
