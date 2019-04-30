b2a58738776b0c4682f6f3b2ec2f109b44fa1c91
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-30 11:38:41 -0600

    radio buttons added using npm package made for react-native w/ animations

--- wild5/src/screens/TrackingForm.js
+++ wild5/src/screens/TrackingForm.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
+
+var radio_props = [
+    {label: 'Yes', value: 1 },
+    {label: 'No', value: 0 }
+  ];
 class TrackingForm extends Component{
     state = {
         exercise: '',
@@ -13,13 +19,15 @@ class TrackingForm extends Component{
     
 
     render(){
+        let screenwidth = Dimensions.get('window').width;
         let screenheight = Dimensions.get('window').height;
         return (
-            <ScrollView pagingEnabled={true}>
+            <ScrollView pagingEnabled={true} horizontal={true}>
             <View style={{
                     backgroundColor: '#333',
                     flex: 1,
                     height: screenheight,
+                    width: screenwidth,
                     justifyContent: 'center',
                     alignItems: 'center'
                 }}>
@@ -38,9 +46,10 @@ class TrackingForm extends Component{
                 </View>
 
                 <View style={{
-                    backgroundColor: '#5f9ea0',
+                    backgroundColor: '#7ac142',
                     flex: 1,
                     height: screenheight,
+                    width: screenwidth,
                     justifyContent: 'center',
                     alignItems: 'center'
                 }}>
@@ -49,13 +58,183 @@ class TrackingForm extends Component{
                     padding: 50,
                     textAlign: 'center',
                     color: 'white'
-                    }}>Exercise Lesson I</Text>
+                    }}>Exercise</Text>
                     <Text style={{
                     fontSize: 40,
                     padding: 15,
                     marginBottom: 200,
                     textAlign: 'center',
-                    color: 'white'}}>FID</Text>
+                    color: 'white'}}>Did I exercise today following the FID principles?</Text>
+
+                    <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    formHorizontal={false}
+                    labelHorizontal={true}
+                    buttonColor={'#5a8f30'}
+                    animation={true}
+                    onPress={(value) => {this.setState({exercise:value})}}
+                    />
+                </View>
+
+                <View style={{
+                    backgroundColor: '#49b8ea',
+                    flex: 1,
+                    height: screenheight,
+                    width: screenwidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <Text style={{
+                    fontSize: 30,
+                    padding: 50,
+                    textAlign: 'center',
+                    color: 'white'
+                    }}>Mindfulness</Text>
+                    <Text style={{
+                    fontSize: 40,
+                    padding: 15,
+                    marginBottom: 200,
+                    textAlign: 'center',
+                    color: 'white'}}>Did I mindfully meditate at least 10 mnutes today?</Text>
+
+                    <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    formHorizontal={false}
+                    labelHorizontal={true}
+                    buttonColor={'#35879d'}
+                    animation={true}
+                    onPress={(value) => {this.setState({mindfulness:value})}}
+                    />
+                </View>
+
+                <View style={{
+                    backgroundColor: '#ba3992',
+                    flex: 1,
+                    height: screenheight,
+                    width: screenwidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <Text style={{
+                    fontSize: 30,
+                    padding: 50,
+                    textAlign: 'center',
+                    color: 'white'
+                    }}>Sleep</Text>
+                    <Text style={{
+                    fontSize: 40,
+                    padding: 15,
+                    marginBottom: 200,
+                    textAlign: 'center',
+                    color: 'white'}}>Did I implement 4 or more of the 6 sleep hygiene practices?</Text>
+
+                    <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    formHorizontal={false}
+                    labelHorizontal={true}
+                    buttonColor={'#8e2762'}
+                    animation={true}
+                    onPress={(value) => {this.setState({sleep:value})}}
+                    />
+                </View>
+
+                <View style={{
+                    backgroundColor: '#ed3833',
+                    flex: 1,
+                    height: screenheight,
+                    width: screenwidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <Text style={{
+                    fontSize: 30,
+                    padding: 50,
+                    textAlign: 'center',
+                    color: 'white'
+                    }}>Connectedness</Text>
+                    <Text style={{
+                    fontSize: 40,
+                    padding: 15,
+                    marginBottom: 200,
+                    textAlign: 'center',
+                    color: 'white'}}>Did I socially connect with at least 2 people today?</Text>
+
+                    <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    formHorizontal={false}
+                    labelHorizontal={true}
+                    buttonColor={'#b32824'}
+                    animation={true}
+                    onPress={(value) => {this.setState({connectedness:value})}}
+                    />
+                </View>
+
+                <View style={{
+                    backgroundColor: '#f3983e',
+                    flex: 1,
+                    height: screenheight,
+                    width: screenwidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <Text style={{
+                    fontSize: 30,
+                    padding: 50,
+                    textAlign: 'center',
+                    color: 'white'
+                    }}>Nutrition</Text>
+                    <Text style={{
+                    fontSize: 40,
+                    padding: 15,
+                    marginBottom: 100,
+                    textAlign: 'center',
+                    color: 'white'}}>Did I log my meals, snacks, and beverages, including alcohol today?</Text>
+
+                    <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    formHorizontal={false}
+                    labelHorizontal={true}
+                    buttonColor={'#b9702c'}
+                    animation={true}
+                    onPress={(value) => {this.setState({nutrition:value})}}
+                    />
+                </View>
+
+                <View style={{
+                    backgroundColor: '#333',
+                    flex: 1,
+                    height: screenheight,
+                    width: screenwidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <Text style={{
+                    fontSize: 30,
+                    padding: 50,
+                    textAlign: 'center',
+                    color: 'white'
+                    }}>HERO</Text>
+                    <Text style={{
+                    fontSize: 40,
+                    padding: 15,
+                    marginBottom: 200,
+                    textAlign: 'center',
+                    color: 'white'}}>Did I complete my HERO exercises today?</Text>
+
+                    <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    formHorizontal={false}
+                    labelHorizontal={true}
+                    buttonColor={'white'}
+                    animation={true}
+                    onPress={(value) => {this.setState({HERO:value})}}
+                    />
                 </View>
             </ScrollView>
         )
