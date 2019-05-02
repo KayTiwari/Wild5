95270a28161e47104c3792d67b0dd32fb5d8ee7c
William Violette <violettewj@gmail.com>
2019-05-02 13:32:45 -0600

    test push

--- wild5/src/screens/LoginForm.js
+++ wild5/src/screens/LoginForm.js
@@ -6,8 +6,8 @@ import {withAuthProvider} from '../context/authcontext';
 import { Actions } from 'react-native-router-flux';
 import RegisterModal from '../modals/RegisterModal';
 import ForgotPassModal from '../modals/ForgotPassModal';
-import abstractimg from '/Users/abhiktiwari/dev/wild5/wild5/src/images/abstract2.jpeg'
-import wild5title from '/Users/abhiktiwari/dev/wild5/wild5/src/images/wild-5-logo-r-color.png'
+import abstractimg from '/Users/bill/dev/React_native/Wild5/Wild5/wild5/src/images/abstract2.jpeg'
+import wild5title from '/Users/bill/dev/React_native/Wild5/Wild5/wild5/src/images/wild-5-logo-r-color.png'
 
 
 class LoginForm extends Component {
