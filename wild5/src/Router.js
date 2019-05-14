a3ff603a5bc8d920280a383730336209f4b5cb63
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-14 12:59:37 -0600

    adding chart and moved router initial

--- wild5/src/Router.js
+++ wild5/src/Router.js
@@ -18,7 +18,7 @@ const Routercomponent = () => {
     <Router>
    <Scene key="root">
     <Scene key="loginform" component={LoginForm} header={null}/>
-        <Scene key="landing" component={Landing} title='Welcome to Wellness' backTitle='Logout' initial/>
+        <Scene key="landing" component={Landing} title='Welcome to Wellness' backTitle='Logout'/>
           <Scene key="account" component={Account} title='Account' header={null}/>
           <Scene key="edroadmap" component={EducationWellnessRoadMap} backTitle='Back' title='Wellness Roadmap'/>
           <Scene key="exercise1" component={ExerciseI} title='Exercise I' backTitle='Back'/>
@@ -28,7 +28,7 @@ const Routercomponent = () => {
           <Scene key='help' component={Help} header={null}/>
           <Scene key='about' component={About} header={null}/>
           <Scene key='faq' component={FAQ} header={null}/>
-          <Scene key='trackingform' component={TrackingForm} title='Wellness Tracking Form' header={null}/>
+          <Scene key='trackingform' component={TrackingForm} title='Wellness Tracking Form' header={null} initial/>
           <Scene key='nutritionquest' component={NutritionQuest} title="Nutrition Quest" header={null} />
           <Scene key='feedback' component={Feedback} title="Feedback" header={null} />
           <Scene key='profile' backTitle='Back' title='Your Profile' component={Profile}/>
