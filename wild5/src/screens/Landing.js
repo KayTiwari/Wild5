3446342318ebaae0f543b6f90834d03851258c14
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-16 18:35:13 -0600

    Moved navbar to it's own component

--- wild5/src/screens/Landing.js
+++ wild5/src/screens/Landing.js
@@ -17,52 +17,10 @@ class Landing extends Component {
       <View style={{marginBottom: 170}}>
       <Carousel />
       </View>
-      {this.state.account === 0 ? <Footer style={{bottom: 0}}>
-          <FooterTab>
-            <Button onPress={() => this.setState({account: 1})}>
-            <Icon name={'ios-pulse'} />
-              <Text>Account</Text>
-            </Button>
-            <Button onPress={() => Actions.edroadmap()}>
-              <Icon name={"compass"} />
-              <Text>RoadMap</Text>
-            </Button>
-            <Button onPress={() => Actions.kickstart()}>
-            <Icon name={"rocket"} />
-              <Text>Kickstart</Text>
-            </Button>
-            <Button onPress={() => Actions.quests()}>
-            <Icon name={"flag"} />
-              <Text>Quests</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-        :
-      <Footer>
-      <FooterTab>
-        <Button onPress={() => this.setState({account: 0})}>
-          <Icon name={'ios-home'} />
-          <Text>Home</Text>
-        </Button>
-        <Button onPress={() => Actions.profile()}>
-        <Icon name={'ios-contacts'} />
-          <Text>Profile</Text>
-        </Button>
-        <Button onPress={() => Actions.help()}>
-        <Icon name={'md-help'} />
-          <Text>Help</Text>
-        </Button>
-        <Button onPress={() => Actions.feedback()}>
-        <Icon name={'ios-chatboxes'} />
-          <Text style={{fontSize: 9}}>Feedback</Text>
-        </Button>
-        <Button onPress={() => Actions.settings()}>
-        <Icon name={'ios-settings'} />
-          <Text style={{fontSize: 10}}>Settings</Text>
-        </Button>
-      </FooterTab>
-    </Footer>}
-    </Container>
+      <View>
+      <Navbar />
+      </View>
+      </Container>
     )
   }
 }
