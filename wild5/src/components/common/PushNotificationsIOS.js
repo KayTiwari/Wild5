bd0cea70ffbfe2032825550ec29ecc61f406cb09
William Violette <violettewj@gmail.com>
2019-06-07 13:33:45 -0600

    enabled background mode

---
+++ wild5/src/components/common/PushNotificationsIOS.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import PushNotification from 'react-native-push-notification';
+
+class PushNotificationsIOS extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+
+export default PushNotificationsIOS
