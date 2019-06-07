aca02df862176ac0da148cf757be9a4fd4463182
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-07 14:21:27 -0600

    added prop passing to bargraph in stats

--- wild5/src/screens/Statistics.js
+++ wild5/src/screens/Statistics.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
+import BarGraph from '../components/charts/BarGraph'
 
 class Statistics extends Component{
     state = {
-
+        exval: 0,
+        mindval: 0,
+        sleepval: 0,
+        socval: 0,
+        nutrval: 0,
     }
-    componentDidMount{
-        
+    componentDidMount(){
+        //firebase read-> data->store as state-> pass as prop
     }
 
     render(){
         return(
-            <View></View>
+            <View>
+                <BarGraph exval={this.state.exval} mindval={this.state.mindval} sleepval={this.state.sleepval} socval={this.state.socval} nutrval={this.state.nutrval} />
+            </View>
         )
     }
 }
