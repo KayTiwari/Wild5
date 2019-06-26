b0c584e53c376959330cfe80c2b5a825f07f47d6
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-26 10:51:56 -0600

    fixed data disappearing and only using one context file now

--- wild5/src/context/authcontext.js
+++ wild5/src/context/authcontext.js
@@ -1,39 +1,85 @@
-import React, {Component} from 'react'
-import firebase from 'firebase'
+import React, { Component } from "react";
+import firebase from "firebase";
 const { Consumer, Provider } = React.createContext();
 
-
-
 export default class AuthProvider extends Component {
-    state = {
-      user: '',
-    }
+  state = {
+    user: "",
+    ready: false
+  };
 
-    componentDidMount(){
-      firebase.initializeApp({
-          apiKey: "AIzaSyC93k0KGpd8myVQxCTgWPw6Qk9NzNA6b_o",
-          authDomain: "wild5-5ca8b.firebaseapp.com",
-          databaseURL: "https://wild5-5ca8b.firebaseio.com",
-          projectId: "wild5-5ca8b",
-          storageBucket: "wild5-5ca8b.appspot.com",
-          messagingSenderId: "714885268112"
-        })
-    }
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC93k0KGpd8myVQxCTgWPw6Qk9NzNA6b_o",
+      authDomain: "wild5-5ca8b.firebaseapp.com",
+      databaseURL: "https://wild5-5ca8b.firebaseio.com",
+      projectId: "wild5-5ca8b",
+      storageBucket: "wild5-5ca8b.appspot.com",
+      messagingSenderId: "714885268112"
+    });
+  }
 
-    
-
-    render() {
-        return (
-          <Provider value={{
-            ...this.state
-          }}>
-            {this.props.children}
-          </Provider>
-        )
-      }
+  getUser = () => {
+    var user = firebase.auth().currentUser;
+    if (user) {
+      var res = user.email.split(".");
+      var userEm = res[0].toString();
+      this.setState({
+        user: userEm
+      });
+      console.log(this.state);
+    } else {
+      console.log("noperz");
     }
-      export function withAuthProvider(C) {
-        return props => <Consumer>
-          {value => <C {...value}{...props} />}
-        </Consumer>
+    var today = new Date();
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    var dateTime = date;
+    this.setState({
+      date: dateTime
+    });
+  };
+
+  getTrackingData = () => {
+    var database = firebase.database();
+    var ref = database.ref(`Surveys/${this.state.user}/${this.state.date}`);
+    ref.on("value", this.gotData, this.errData);
+  }
+
+  gotData = data => {
+    newData = data.val();
+    this.setState({
+      ran: true,
+      trackdata: newData || {}
+    }, () => {
+      this.setState({
+        ready: true
+      })
+    });
+  }
+
+  errData = err => {
+    console.log(err);
+  }
+
+  render() {
+    return (
+      <Provider
+        value={{
+          getUser: this.getUser,
+          getTrackingData: this.getTrackingData,
+          ...this.state
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+export function withAuthProvider(C) {
+  return props => <Consumer>{value => <C {...value} {...props} />}</Consumer>;
 }
--- wild5/src/screens/NewLoginScreen.js
+++ wild5/src/screens/NewLoginScreen.js
@@ -1,118 +1,232 @@
-import React, {Component} from 'react';
-import { View, Image, ImageBackground, Dimensions, ScrollView } from 'react-native';
-import { Text, Spinner } from 'native-base';
-import ForgotModal from '../modals/NewForgotModal';
-import firebase from 'firebase'
-import {withAuthProvider} from '../context/authcontext';
-import { Actions } from 'react-native-router-flux';
-import abstractimg from '../images/abstract2.jpeg';
-import wild5title from '../images/wild-5-logo-r-color.png'
+import React, { Component } from "react";
+import {
+  View,
+  Image,
+  ImageBackground,
+  Dimensions,
+  ScrollView
+} from "react-native";
+import { Text, Spinner } from "native-base";
+import ForgotModal from "../modals/NewForgotModal";
+import firebase from "firebase";
+import { withAuthProvider } from "../context/authcontext";
+import { Actions } from "react-native-router-flux";
+import abstractimg from "../images/abstract2.jpeg";
+import wild5title from "../images/wild-5-logo-r-color.png";
 // import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button } from 'react-native-elements';
+import { Input, Button } from "react-native-elements";
 // import console = require('console');
 
-const screenheight = Dimensions.get('window').height;
+const screenheight = Dimensions.get("window").height;
 
-class NewLoginScreen extends Component{
-    state = {
-        email: '',
-        password: '',
-        raised: true,
-        error: '',
-        loading: false,
-        modal: false
-    }
+class NewLoginScreen extends Component {
+  state = {
+    email: "",
+    password: "",
+    raised: true,
+    error: "",
+    loading: false,
+    modal: false
+  };
 
-    LoginPress() {
-        const { email, password } = this.state;
-        this.setState({
-            error: '',
-            loading: true
-        })
-        firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(() => {
-            this.onLoginSuccess();
-        })
-        .catch(() => {
-                    this.onLoginFail();
-                });
-    };
+  LoginPress() {
+    const { email, password } = this.state;
+    this.setState({
+      error: "",
+      loading: true
+    });
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.onLoginSuccess();
+      })
+      .catch(() => {
+        this.onLoginFail();
+      });
+  }
 
-    onLoginSuccess() {
-        Actions.landing();
-    }
+  onLoginSuccess() {
+    Actions.landing();
+    this.props.getUser();
+  }
 
-    onLoginFail() {
-        this.setState({
-            error: 'Authentication failed',
-        })
-    }
+  onLoginFail() {
+    this.setState({
+      error: "Authentication failed"
+    });
+  }
 
-    render(){
-        return (
-        <ScrollView>
-        <View style={{backgroundColor: 'white', height: screenheight}}>
-            {this.state.modal ? <ForgotModal isVisible={this.state.modal}/>: null}
-            <ImageBackground source={abstractimg} style={{flex: 1, resizeMode: 'cover', height: screenheight*.6, width: '100%'}}>
-            <View style={{width: '80%', alignSelf: 'center', marginTop: '10%'}}><Image source={wild5title} style={{resizeMode: 'contain', width: '100%', marginTop: '15%'}}/></View> 
-            
-            <View style={{backgroundColor:'white', height:'25%', width:'80%', padding:30, borderRadius:5, position:'absolute', top:'40%', alignSelf:'center' }}>
-            <Input
-                placeholder='Email'
-                onChangeText={(email) => this.setState({email}) }
+  render() {
+    return (
+      <ScrollView>
+        <View style={{ backgroundColor: "white", height: screenheight }}>
+          {this.state.modal ? (
+            <ForgotModal isVisible={this.state.modal} />
+          ) : null}
+          <ImageBackground
+            source={abstractimg}
+            style={{
+              flex: 1,
+              resizeMode: "cover",
+              height: screenheight * 0.6,
+              width: "100%"
+            }}
+          >
+            <View
+              style={{ width: "80%", alignSelf: "center", marginTop: "10%" }}
+            >
+              <Image
+                source={wild5title}
+                style={{
+                  resizeMode: "contain",
+                  width: "100%",
+                  marginTop: "15%"
+                }}
+              />
+            </View>
+
+            <View
+              style={{
+                backgroundColor: "white",
+                height: "25%",
+                width: "80%",
+                padding: 30,
+                borderRadius: 5,
+                position: "absolute",
+                top: "40%",
+                alignSelf: "center"
+              }}
+            >
+              <Input
+                placeholder="Email"
+                onChangeText={email => this.setState({ email })}
                 spellCheck={false}
+                keyboardType={"email-address"}
+                autoCapitalize={"none"}
                 textContentType={"emailAddress"}
                 shake={true}
-            />
-            <Input
-                placeholder='Password'
-                onChangeText={(password) => this.setState({password}) }
+              />
+              <Input
+                placeholder="Password"
+                onChangeText={password => this.setState({ password })}
                 spellCheck={false}
                 textContentType={"password"}
+                autoCapitalize={"none"}
                 secureTextEntry={true}
                 shake={true}
-                inputContainerStyle={{backgroundColor: 'white', borderLeftColor:'white', borderRightColor:'white', borderBottomWidth:0, borderWidth: 1}}
+                inputContainerStyle={{
+                  backgroundColor: "white",
+                  borderLeftColor: "white",
+                  borderRightColor: "white",
+                  borderBottomWidth: 0,
+                  borderWidth: 1
+                }}
+              />
+            </View>
+          </ImageBackground>
+          <Text
+            style={{
+              position: "absolute",
+              top: "55%",
+              width: "50%",
+              marginBottom: 0,
+              alignSelf: "center",
+              color: "red"
+            }}
+          >
+            {this.state.error}
+          </Text>
+          {this.state.loading && !this.state.error ? (
+            <View
+              style={{
+                position: "absolute",
+                top: "55%",
+                width: "50%",
+                marginBottom: 0,
+                alignSelf: "center"
+              }}
+            >
+              <Spinner />
+            </View>
+          ) : null}
+          <View
+            style={{
+              position: "absolute",
+              top: "65%",
+              width: "50%",
+              marginTop: 0,
+              alignSelf: "center"
+            }}
+          >
+            <Button
+              title="Login"
+              type="outline"
+              raised={this.state.raised}
+              onPress={() => this.LoginPress()}
             />
-            </View>
-            </ImageBackground> 
-            <Text style={{position:'absolute', top:'55%', width:'50%', marginBottom:0, alignSelf:'center', color: 'red'}}>{this.state.error}</Text>
-            {this.state.loading && !this.state.error ? <View style={{position:'absolute', top:'55%', width:'50%', marginBottom:0, alignSelf:'center'}}><Spinner /></View>: null}
-            <View style={{position:'absolute', top:'65%', width:'50%', marginTop: 0, alignSelf:'center'}}>
+          </View>
+          <View
+            style={{
+              position: "absolute",
+              top: "73%",
+              width: "50%",
+              alignSelf: "center"
+            }}
+          >
+            <Button
+              title="Register"
+              type="outline"
+              raised={this.state.raised}
+              onPress={() => Actions.registerpage()}
+            />
+          </View>
 
-
-                <Button 
-                title='Login'
-                type='outline'
-                raised={this.state.raised}
-                onPress={() => this.LoginPress()}
-                />
-            </View>
-            <View style={{position:'absolute', top:'73%', width:'50%', alignSelf:'center'}}>
-                <Button 
-                title='Register'
-                type='outline'
-                raised={this.state.raised}
-                onPress={() => Actions.registerpage()}
-                />
-            </View>
-
-            <View style={{position:'absolute', top:'80%', width:'50%', alignSelf:'center'}}>
-                <Button 
-                title='Forgot password?'
-                onPress={() => this.setState({modal: !this.state.modal})}
-                type='clear'
-                titleStyle={{fontSize:15}}
-                />
-            </View>
+          <View
+            style={{
+              position: "absolute",
+              top: "80%",
+              width: "50%",
+              alignSelf: "center"
+            }}
+          >
+            <Button
+              title="Forgot password?"
+              onPress={() => this.setState({ modal: !this.state.modal })}
+              type="clear"
+              titleStyle={{ fontSize: 15 }}
+            />
+          </View>
         </View>
 
-            <View style={{backgroundColor:'#333'}}>
-                <Text style={{color: 'white', fontSize:15, fontWeight:'600', textAlign:'center', marginTop:'10%'}}>App made on React Native by the Wild Card Team.</Text>
-                <Text style={{color: 'white', fontSize:15, fontWeight:'600', textAlign:'center', marginBottom:'5%'}}>Wild5 and all its resource materials are a product of the works by Rakesh and Saundra Jain MD.</Text>
-            </View>
-        </ScrollView>
-        )
-    }
+        <View style={{ backgroundColor: "#333" }}>
+          <Text
+            style={{
+              color: "white",
+              fontSize: 15,
+              fontWeight: "600",
+              textAlign: "center",
+              marginTop: "10%"
+            }}
+          >
+            App made on React Native by the Wild Card Team.
+          </Text>
+          <Text
+            style={{
+              color: "white",
+              fontSize: 15,
+              fontWeight: "600",
+              textAlign: "center",
+              marginBottom: "5%"
+            }}
+          >
+            Wild5 and all its resource materials are a product of the works by
+            Rakesh and Saundra Jain MD.
+          </Text>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 export default withAuthProvider(NewLoginScreen);
