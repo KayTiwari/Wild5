b993ca9e5b8751fe194a2859011dbedd4ea51cd8
Kasey Cowley <kchunterdeluxe@gmail.com>
2019-07-01 21:42:48 -0600

    Update Carousel UI

--- wild5/src/components/Carousel.js
+++ wild5/src/components/Carousel.js
@@ -1,58 +1,65 @@
 import React from 'react';
-import {StyleSheet, Text, Image, View, SafeAreaView} from 'react-native';
-import Carousel from 'react-native-snap-carousel';
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      carouselItems: [
-        {
-          id: 1,
-          title: 'Words of Advice',
-        },
-        {
-          id: 2,
-          title: `"Perfection is not the goal, set your goals high and do your best everyday!"`,
-        },
-        {
-          id: 3,
-          title: `"Be kind to yourself as you begin making these changes.  Change is never easy."`,
-        },
-        {
-          id: 4,
-          title: `"If you miss a day or 2 shake it off, regroup, and begin again."`,
-        },
-        {
-          title: `"Do not throw in the towel.  Remember to track your daily exercise practices using the participant tracking from"`,
-        },
-      ],
-    };
-  }
-  _renderItem = ({item, index}) => {
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import RNSnapCarousel from 'react-native-snap-carousel';
+import LinearGradient from 'react-native-linear-gradient';
+
+const {width: screenWidth} = Dimensions.get('window');
+
+class Carousel extends React.Component {
+  state = {
+    carouselItems: [
+      {
+        id: 1,
+        title: 'Words of Advice',
+      },
+      {
+        id: 2,
+        title: `"Perfection is not the goal, set your goals high and do your best everyday!"`,
+      },
+      {
+        id: 3,
+        title: `"Be kind to yourself as you begin making these changes.  Change is never easy."`,
+      },
+      {
+        id: 4,
+        title: `"If you miss a day or 2 shake it off, regroup, and begin again."`,
+      },
+      {
+        title: `"Do not throw in the towel.  Remember to track your daily exercise practices using the participant tracking from"`,
+      },
+    ],
+  };
+
+  renderItem = ({item}) => {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text
-          style={
-            this.state.carouselItems[0].id === 1
-              ? {fontSize: 30, color: '#fff'}
-              : {fontSize: 20, color: '#fff'}
-          }
-        >
+      <LinearGradient
+        colors={['#22c1c3', '#0AB1E7']}
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 20,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{color: 'white', fontSize: 20}} adjustsFontSizeToFit>
           {item.title}
         </Text>
-      </View>
+      </LinearGradient>
     );
   };
   render() {
     return (
       <View style={styles.container}>
-        <Carousel
+        <RNSnapCarousel
           data={this.state.carouselItems}
-          sliderWidth={250}
+          sliderWidth={screenWidth}
           itemWidth={250}
-          renderItem={this._renderItem}
+          renderItem={this.renderItem}
           autoplay={true}
+          autoplayInterval={5000}
           loop={true}
+          layout={'default'}
         />
       </View>
     );
@@ -60,14 +67,10 @@ class App extends React.Component {
 }
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    height: 250,
-    backgroundColor: '#0AB1E7',
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 10,
-    marginLeft: 20,
-    marginRight: 20,
   },
 });
-export default App;
+
+export default Carousel;
