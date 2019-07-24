663a7f5c9de39e3e69561a3304cbe4e192255923
Kasey Cowley <kchunterdeluxe@gmail.com>
2019-07-23 22:10:26 -0600

    Add react-native-config

--- wild5/android/app/src/main/java/com/wild5/MainApplication.java
+++ wild5/android/app/src/main/java/com/wild5/MainApplication.java
@@ -3,6 +3,7 @@ package com.wild5;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new LinearGradientPackage(),
             new ReactSliderPackage(),
             new RNCameraPackage(),
