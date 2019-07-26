49cc770c5ba9251ac8b011073373e76f4508f714
Kasey Cowley <kchunterdeluxe@gmail.com>
2019-07-25 21:15:27 -0600

    Remove unused imports, prettify

--- wild5/src/screens/trackingscreens/SocialTracking.js
+++ wild5/src/screens/trackingscreens/SocialTracking.js
@@ -1,65 +1,47 @@
-import React, { Component } from "react";
-import { ScrollView, View, Dimensions, ImageBackground } from "react-native";
-import {
-  Item,
-  Label,
-  Text,
-  Content,
-  ListItem,
-  CheckBox,
-  Body,
-  Container,
-  Header
-} from "native-base";
-import { ModButton } from "../../components/common";
-import firebase from "firebase";
-import RadioForm, {
-  RadioButton,
-  RadioButtonInput,
-  RadioButtonLabel
-} from "react-native-simple-radio-button";
-import { Actions } from "react-native-router-flux";
-import socialtracking from "../../images/socialtracking.jpg";
+import React, {Component} from 'react';
+import {Text, Content, ListItem, CheckBox, Body, Container} from 'native-base';
+import {ModButton} from '../../components/common';
+import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
+import socialtracking from '../../images/socialtracking.jpg';
 import {BlurredBackgroundImage} from '../../components/common/BlurredBackgroundImage';
 
-const screenheight = Dimensions.get("window").height;
 class SocialTracking extends Component {
   state = {
     friendcall: false,
     friendinperson: false,
     familycall: false,
-    familyinperson: false
+    familyinperson: false,
   };
 
   checkBox = type => {
-    if (type === "fc") {
+    if (type === 'fc') {
       this.setState({
-        friendcall: !this.state.friendcall
+        friendcall: !this.state.friendcall,
       });
-    } else if (type === "fip") {
+    } else if (type === 'fip') {
       this.setState({
-        friendinperson: !this.state.friendinperson
+        friendinperson: !this.state.friendinperson,
       });
-    } else if (type === "famc") {
+    } else if (type === 'famc') {
       this.setState({
-        familycall: !this.state.familycall
+        familycall: !this.state.familycall,
       });
-    } else if (type === "famip") {
+    } else if (type === 'famip') {
       this.setState({
-        familyinperson: !this.state.familyinperson
+        familyinperson: !this.state.familyinperson,
       });
     }
   };
 
   submitForm() {
-    // console.log(this.state);
     const {
       friendcall,
       friendinperson,
       familycall,
       familyinperson,
       user,
-      date
+      date,
     } = this.state;
     firebase
       .database()
@@ -68,7 +50,7 @@ class SocialTracking extends Component {
         socfriendcall: friendcall,
         socfriendinperson: friendinperson,
         socfamilycall: familycall,
-        socfamilyinperson: familyinperson
+        socfamilyinperson: familyinperson,
       });
     Actions.landing();
   }
@@ -76,24 +58,24 @@ class SocialTracking extends Component {
   componentDidMount() {
     var user = firebase.auth().currentUser;
     if (user) {
-      var res = user.email.split(".");
+      var res = user.email.split('.');
       var userEm = res[0].toString();
       this.setState({
-        user: userEm
+        user: userEm,
       });
     } else {
-      console.log("noperz");
+      console.log('noperz');
     }
     var today = new Date();
     var date =
       today.getFullYear() +
-      "-" +
+      '-' +
       (today.getMonth() + 1) +
-      "-" +
+      '-' +
       today.getDate();
     var dateTime = date;
     this.setState({
-      date: dateTime
+      date: dateTime,
     });
   }
 
@@ -101,21 +83,21 @@ class SocialTracking extends Component {
     return (
       <Container>
         <BlurredBackgroundImage
-        style={{paddingHorizontal: 10}}
-        source={socialtracking}
-        blurRadius={20}
-      >
+          style={{paddingHorizontal: 10}}
+          source={socialtracking}
+          blurRadius={20}
+        >
           <Text
             style={{
               fontSize: 30,
-              textAlign: "center",
-              marginTop: "20%",
-              marginBottom: "20%",
-              fontWeight: "600"
+              textAlign: 'center',
+              marginTop: '20%',
+              marginBottom: '20%',
+              fontWeight: '600',
             }}
           >
-            Track your{" "}
-            <Text style={{ color: "red", fontSize: 30, fontWeight: "600" }}>
+            Track your{' '}
+            <Text style={{color: 'red', fontSize: 30, fontWeight: '600'}}>
               Social
             </Text>
           </Text>
@@ -123,57 +105,57 @@ class SocialTracking extends Component {
             <Text
               style={{
                 fontSize: 20,
-                textAlign: "center",
-                marginTop: "10%",
-                marginBottom: "10%",
-                fontWeight: "600"
+                textAlign: 'center',
+                marginTop: '10%',
+                marginBottom: '10%',
+                fontWeight: '600',
               }}
             >
               Which social practices did you complete?
             </Text>
-            <ListItem onPress={() => this.checkBox("fc")}>
+            <ListItem onPress={() => this.checkBox('fc')}>
               <CheckBox
                 color="red"
                 checked={this.state.friendcall}
-                onPress={() => this.checkBox("fc")}
+                onPress={() => this.checkBox('fc')}
               />
               <Body>
                 <Text>Called Friend</Text>
               </Body>
             </ListItem>
-            <ListItem onPress={() => this.checkBox("fip")}>
+            <ListItem onPress={() => this.checkBox('fip')}>
               <CheckBox
                 color="green"
                 checked={this.state.friendinperson}
-                onPress={() => this.checkBox("fip")}
+                onPress={() => this.checkBox('fip')}
               />
               <Body>
                 <Text>Met Friend in person</Text>
               </Body>
             </ListItem>
-            <ListItem onPress={() => this.checkBox("famc")}>
+            <ListItem onPress={() => this.checkBox('famc')}>
               <CheckBox
                 color="blue"
                 checked={this.state.familycall}
-                onPress={() => this.checkBox("famc")}
+                onPress={() => this.checkBox('famc')}
               />
               <Body>
                 <Text>Called Family</Text>
               </Body>
             </ListItem>
-            <ListItem onPress={() => this.checkBox("famip")}>
+            <ListItem onPress={() => this.checkBox('famip')}>
               <CheckBox
                 color="orange"
                 checked={this.state.familyinperson}
-                onPress={() => this.checkBox("famip")}
+                onPress={() => this.checkBox('famip')}
               />
               <Body>
                 <Text>Met Family in person</Text>
               </Body>
             </ListItem>
             <ModButton
-              style={{ alignSelf: "center", textAlign: "center" }}
-              color={"black"}
+              style={{alignSelf: 'center', textAlign: 'center'}}
+              color={'black'}
               onPress={() => this.submitForm()}
               label="Submit"
             >
