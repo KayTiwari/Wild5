8b2f09ccd54e107da89e156b05e777a24babba0d
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-07-28 11:44:54 -0600

    changed HERO feedback

--- wild5/src/Router.js
+++ wild5/src/Router.js
@@ -83,8 +83,8 @@ const Routercomponent = () => {
           <Scene key='exercisetracking' backTitle='Back' title='Exercise Tracking' component={ExerciseTracking} />
           <Scene key='mindfulnesstracking' backTitle='Back' title='Mindfulness Tracking' component={MindfulnessTracking} />
           <Scene key='sleeptracking' backTitle='Back' title='Sleep Tracking' component={SleepTracking}  />
-          <Scene key='socialtracking' backTitle='Back' title='Social Tracking' component={SocialTracking} initial/>
-          <Scene key='nutritiontracking' backTitle='Back' title='Nutrition Tracking' component={NutritionTracking}/>
+          <Scene key='socialtracking' backTitle='Back' title='Social Tracking' component={SocialTracking} />
+          <Scene key='nutritiontracking' backTitle='Back' title='Nutrition Tracking' component={NutritionTracking} initial/>
           <Scene key='progress' component={Progress} backTitle='Back' title="Today's Progress"/>
           <Scene key='herointro' component={Herointro} backTitle='Back' title="Today's HERO"/>
           <Scene key='herohappy' component={HeroHappy} backTitle='Back' title="Happiness"/>
