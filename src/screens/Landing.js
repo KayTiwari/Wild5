89fe382d267eda15cbba362e8838e1bba9e52068
Kasey Cowley <kchunterdeluxe@gmail.com>
2019-08-18 22:19:56 -0600

    Convert screen to functional component

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,31 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View, ScrollView, SafeAreaView, Image} from 'react-native';
 import wild5title from '../images/wild-5-logo-r-color.png';
 import Navbar from '../components/Navbar';
 import Navigation from '../components/LandingNavigation';
 
-export default class Landing extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <SafeAreaView style={{flex: 1}}>
-          <ScrollView bounces={false}>
-            <Image
-              source={wild5title}
-              style={{
-                width: '80%',
-                resizeMode: 'contain',
-                marginTop: '5%',
-                alignSelf: 'center',
-              }}
-            />
-            <View style={{marginTop: '5%', flex: 1}}>
-              <Navigation />
-            </View>
-          </ScrollView>
-        </SafeAreaView>
-        <Navbar homedisable />
-      </View>
-    );
-  }
+export default function Landing() {
+  return (
+    <View style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1}}>
+        <ScrollView bounces={false}>
+          <Image
+            source={wild5title}
+            style={{
+              width: '80%',
+              resizeMode: 'contain',
+              marginTop: '5%',
+              alignSelf: 'center',
+            }}
+          />
+          <View style={{marginTop: '5%', flex: 1}}>
+            <Navigation />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      <Navbar homedisable />
+    </View>
+  );
 }
