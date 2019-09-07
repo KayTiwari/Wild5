bbbfaf6ec77e3d624c3b3ea9aafbe32c34276f11
William Violette <violettewj@gmail.com>
2019-09-07 15:02:32 -0600

    updated about page

--- src/screens/accountmenu/Help.js
+++ src/screens/accountmenu/Help.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, SafeAreaView } from "react-native";
 import { Accordion } from 'native-base'
 import Navbar from "../../components/Navbar";
 import { aboutData }from '../../components/common/aboutData'
@@ -9,10 +9,12 @@ const Help = () => {
     <View
       style={{flex: 1}}
     >
+    <SafeAreaView style={{flex: 1}}>
       <Accordion dataArray={aboutData} expanded={4} headerStyle={{ backgroundColor: "#fff" }}
-            contentStyle={{ backgroundColor: "#fff", color: "#000" }}/>
+            contentStyle={{ backgroundColor: "#fff", color: "#000", lineHeight: Platform.OS === 'ios' ? 23 : null }}/>
       
         <Navbar faqdisable/>
+        </SafeAreaView>
     </View>
   );
 };
