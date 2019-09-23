e1c017c794103cf117e900309feff4362feb7050
William Violette <violettewj@gmail.com>
2019-09-23 09:05:14 -0600

    login screen fixes

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,145 +1,242 @@
-import React, {useState} from 'react'
-import { View, Text, TouchableOpacity, ActivityIndicator, KeyboardAvoidingView , Alert} from 'react-native'
-import {Icon, Input } from 'native-base'
-import firebase from 'react-native-firebase'
-import {withAuthProvider} from '../context/authcontext'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  Alert,
+  StyleSheet,
+  TouchableWithoutFeedback
+} from "react-native";
+import { Icon, Input } from "native-base";
+import firebase from "react-native-firebase";
+import { withAuthProvider } from "../context/authcontext";
 
-
-const LoginModal = (props) => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const LoginModal = props => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [raised, setRaised] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  const [forgotPW, setForgotPW] = useState(false)
-
+  const [forgotPW, setForgotPW] = useState(false);
 
   LoginPress = () => {
-    setLoading(true);
-    setError('');
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {
-        console.log("logged in")
-      })
-      .catch(err => {
-        this.onLoginFail(err);
-      });
+    if (email === "" && password === "") {
+      setError("Please enter your email and password");
+    } else if (email === "") {
+      setError("Please enter your email");
+    } else if (password === "") {
+      setError("Please enter your password");
+    } else {
+      setLoading(true);
+      setError("");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+          console.log("logged in");
+        })
+        .catch(err => {
+          this.onLoginFail(err);
+        });
+    }
   };
 
   closeModal = () => {
-    return props.closeModal()
-  }
+    return props.closeModal();
+  };
 
   forgotPress = () => {
-    setLoading(true)
+    setLoading(true);
     firebase
       .auth()
       .sendPasswordResetEmail(email)
       .then(() => {
-        Alert.alert(
-          'Success, Password Reset! Check Your Email',''
-          ,
-          [
-            {
-              text: 'ok',
-              onPress: ()=> closeModal(),
-              style: 'ok',
-            }
-          ],
-        )
+        Alert.alert("Success, Password Reset! Check Your Email", "", [
+          {
+            text: "ok",
+            onPress: () => closeModal(),
+            style: "ok"
+          }
+        ]);
       })
-      .catch((err) => {
-        setError(err)
+      .catch(err => {
+        setError(err);
       });
   };
 
-
   onLoginFail = err => {
     console.log(err.message);
-    if (err.message === 'The email address is badly formatted') {
-      setError('Invalid Email Address');
+    if (err.message === "The email address is badly formatted") {
+      setError("Invalid Email Address");
     } else if (
       err.message ===
-      'There is no user record corresponding to this identifier. The user may have been deleted.'
+      "There is no user record corresponding to this identifier. The user may have been deleted."
     ) {
-      setError('Email address not registered yet!');
+      setError("Email address not registered yet!");
     } else if (
       err.message ===
-      'The password is invalid or the user does not have a password.'
+      "The password is invalid or the user does not have a password."
     ) {
-      setError('Wrong Password');
+      setError("Wrong Password");
     } else {
       setError(err.message);
     }
   };
 
-    return (
-      <KeyboardAvoidingView
+  return (
+    <TouchableWithoutFeedback onPress={()=> props.closeModal()}>
+    <KeyboardAvoidingView
       style={{ flex: 1 }}
       behavior={Platform.OS === "ios" ? "padding" : null}
       enabled
     >
-        <View style={{ flex: 1, justifyContent:'center' }}>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <View
+          style={{backgroundColor: "#fff",
+          height:
+            !forgotPW && !loading
+              ? 320
+              : !forgotPW && loading
+              ? 410
+              : forgotPW && loading
+              ? 250
+              : 200,
+          marginLeft: "5%",
+          marginRight: "5%",
+          marginTop: "5%",
+          borderRadius: 20,
+          shadowColor: "black",
+          shadowOffset: { width: 4, height: 4 },
+          shadowOpacity: 0.8,
+          shadowRadius: 6}}
+        >
           <View
-            style={{
-              backgroundColor:'#fff',
-              height: !forgotPW && !loading ? 320 : !forgotPW && loading ? 410  : forgotPW && loading ? 250 : 200,
-              marginLeft: "5%",
-              marginRight: "5%",
-              marginTop: "5%",
-              borderRadius: 20,
-              shadowColor: "black",
-              shadowOffset: { width: 4, height: 4 },
-              shadowOpacity: 0.8,
-              shadowRadius: 6
-            }}
+            style={styles.inputContainer}
           >
-          <View style={{alignSelf:'center', marginTop:'10%', height: 50, width:'80%', borderBottomWidth:2, borderColor: '#041D5D'}}>
-          <Input
+            <Input
               placeholder="Email"
               onChangeText={text => setEmail(text)}
               spellCheck={false}
-              keyboardType={'email-address'}
-              autoCapitalize={'none'}
-              textContentType={'emailAddress'}
+              keyboardType={"email-address"}
+              autoCapitalize={"none"}
+              textContentType={"emailAddress"}
               shake={true}
             />
           </View>
-         {!forgotPW ? <View style={{alignSelf:'center', marginTop:'10%',height: 50, width:'80%', borderBottomWidth:2, borderColor: '#041D5D'}}>
-          <Input
-              placeholder="Password"
-              onChangeText={text => setPassword(text)}
-              spellCheck={false}
-              textContentType={'password'}
-              autoCapitalize={'none'}
-              secureTextEntry={true}
-              shake={true}
-              inputContainerStyle={{
-                backgroundColor: 'white',
-                borderLeftColor: Platform.OS === 'ios' ? 'white' : null,
-                borderRightColor: Platform.OS === 'ios' ? 'white' : null,
-                borderBottomWidth: 0,
-                borderWidth: 1,
+          {!forgotPW ? (
+            <View
+              style={{
+                alignSelf: "center",
+                marginTop: "10%",
+                height: 50,
+                width: "80%",
+                borderBottomWidth: 2,
+                borderColor: "#041D5D"
               }}
-            />
-          </View> : null}
-          {loading ? <View style={{alignSelf:'center', paddingTop:10}}><ActivityIndicator size="large" color="#041D5D"/></View> : null}
-          <View style={{alignSelf:'center', width:'90%', marginTop:'5%'}}>
-            {!forgotPW ? <TouchableOpacity style={{height: 70, width: "80%", borderRadius:10, backgroundColor:'#041D5D', alignSelf:'center', justifyContent:'center'}} onPress={()=> LoginPress()}>
-              <Text style={{alignSelf: 'center', color:'#fff', fontSize:26, fontWeight:"800"}}>Login</Text>
-            </TouchableOpacity> : <TouchableOpacity style={{height: 70, width: "80%", borderRadius:10, backgroundColor:'#041D5D', alignSelf:'center', justifyContent:'center'}} onPress={()=> forgotPress()}>
-              <Text style={{alignSelf: 'center', color:'#fff', fontSize:26, fontWeight:"800"}}>Reset Password</Text>
-            </TouchableOpacity>}</View>
-            {!forgotPW ? <TouchableOpacity style={{alignSelf:'center', marginTop: 10}} onPress={()=> setForgotPW(true)}>
-              <Text style={{color:'#041D5D'}}>Forgot Password?</Text>
-            </TouchableOpacity> : null}
+            >
+              <Input
+                placeholder="Password"
+                onChangeText={text => setPassword(text)}
+                spellCheck={false}
+                textContentType={"password"}
+                autoCapitalize={"none"}
+                secureTextEntry={true}
+                shake={true}
+                inputContainerStyle={{
+                  backgroundColor: "white",
+                  borderLeftColor: Platform.OS === "ios" ? "white" : null,
+                  borderRightColor: Platform.OS === "ios" ? "white" : null,
+                  borderBottomWidth: 0,
+                  borderWidth: 1
+                }}
+              />
+            </View>
+          ) : null}
+          {loading ? (
+            <View style={{ alignSelf: "center", paddingTop: 10 }}>
+              <ActivityIndicator size="large" color="#041D5D" />
+            </View>
+          ) : null}
+          <View style={{ alignSelf: "center", width: "90%", marginTop: "5%" }}>
+          {error ? <Text style={{alignSelf:'center', color:'red'}}>{error}</Text> : null}
+            {!forgotPW ? (
+              <TouchableOpacity
+                style={{
+                  height: 70,
+                  width: "80%",
+                  borderRadius: 10,
+                  backgroundColor: "#041D5D",
+                  alignSelf: "center",
+                  justifyContent: "center"
+                }}
+                onPress={() => LoginPress()}
+              >
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    color: "#fff",
+                    fontSize: 26,
+                    fontWeight: "800"
+                  }}
+                >
+                  Login
+                </Text>
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                style={{
+                  height: 70,
+                  width: "80%",
+                  borderRadius: 10,
+                  backgroundColor: "#041D5D",
+                  alignSelf: "center",
+                  justifyContent: "center"
+                }}
+                onPress={() => forgotPress()}
+              >
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    color: "#fff",
+                    fontSize: 26,
+                    fontWeight: "800"
+                  }}
+                >
+                  Reset Password
+                </Text>
+              </TouchableOpacity>
+            )}
           </View>
-          </View>
-          </KeyboardAvoidingView>
-    )
-}
+          {!forgotPW ? (
+            <TouchableOpacity
+              style={{ alignSelf: "center", marginTop: 10 }}
+              onPress={() => setForgotPW(true)}
+            >
+              <Text style={{ color: "#041D5D" }}>Forgot Password?</Text>
+            </TouchableOpacity>
+          ) : null}
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
+};
 
-export default LoginModal
+export default LoginModal;
+
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    
+  },
+  inputContainer: {
+    alignSelf: "center",
+    marginTop: "10%",
+    height: 50,
+    width: "80%",
+    borderBottomWidth: 2,
+    borderColor: "#041D5D"
+  }
+})
